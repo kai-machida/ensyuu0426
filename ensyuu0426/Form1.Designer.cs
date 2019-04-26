9500ea42561aee21d24a4dcb8c9c1444bda5e91d
kai-machida <49702001+kai-machida@users.noreply.github.com>
2019-04-26 16:07:25 +0900

    作業中

--- ensyuu0426/Form1.Designer.cs
+++ ensyuu0426/Form1.Designer.cs
@@ -35,12 +35,14 @@
             // 
             // button1
             // 
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.button1.Location = new System.Drawing.Point(94, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -50,6 +52,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -59,6 +62,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
--- ensyuu0426/Form1.cs
+++ ensyuu0426/Form1.cs
@@ -16,5 +16,20 @@ namespace ensyuu0426
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.ForeColor = SystemColors.Control;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
